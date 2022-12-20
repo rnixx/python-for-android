47455323aa5e2e83cf6198226e4d9c98d03415ae
Robert Niederreiter <rnix@squarewave.at>
2022-12-20 14:36:44 +0100

    pin setuptools_rust to 1.2.0

--- pythonforandroid/recipes/setuptools_rust/__init__.py
+++ pythonforandroid/recipes/setuptools_rust/__init__.py
@@ -2,7 +2,7 @@ from pythonforandroid.recipe import PythonRecipe
 
 
 class SetuptoolsRustRecipe(PythonRecipe):
-    version = '1.5.2'
+    version = '1.2.0'
     url = 'https://pypi.python.org/packages/source/s/setuptools-rust/setuptools-rust-{version}.tar.gz'
     depends = ['typing_extensions', 'semantic_version']
     call_hostpython_via_targetpython = False
