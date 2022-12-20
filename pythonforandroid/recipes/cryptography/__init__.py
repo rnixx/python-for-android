02cadee02214cb010f67577f554b1b2bcb068f30
Robert Niederreiter <rnix@squarewave.at>
2022-12-20 13:21:32 +0100

    try to suppress rust build

--- pythonforandroid/recipes/cryptography/__init__.py
+++ pythonforandroid/recipes/cryptography/__init__.py
@@ -11,6 +11,8 @@ class CryptographyRecipe(CompiledComponentsPythonRecipe):
     def get_recipe_env(self, arch):
         env = super().get_recipe_env(arch)
 
+        env['CRYPTOGRAPHY_DONT_BUILD_RUST'] = '1'
+
         openssl_recipe = Recipe.get_recipe('openssl', self.ctx)
         env['CFLAGS'] += openssl_recipe.include_flags(arch)
         env['LDFLAGS'] += openssl_recipe.link_dirs_flags(arch)
