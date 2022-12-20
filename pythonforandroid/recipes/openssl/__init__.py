11cbb76d21d567d3dd5a0857047d1d0a63429ae3
Robert Niederreiter <rnix@squarewave.at>
2022-12-20 13:50:42 +0100

    switch back to libopenssl 1.1.1m

--- pythonforandroid/recipes/openssl/__init__.py
+++ pythonforandroid/recipes/openssl/__init__.py
@@ -47,7 +47,7 @@ class OpenSSLRecipe(Recipe):
     version = '1.1'
     '''the major minor version used to link our recipes'''
 
-    url_version = '1.1.1n'
+    url_version = '1.1.1m'
     '''the version used to download our libraries'''
 
     url = 'https://www.openssl.org/source/openssl-{url_version}.tar.gz'
