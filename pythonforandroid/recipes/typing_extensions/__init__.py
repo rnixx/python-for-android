03b4c35f1385dd09c132c2c294f464305c9b3baa
Robert Niederreiter <rnix@squarewave.at>
2022-12-20 12:36:15 +0100

    Use latest typing extensions version containing a setup.py

--- pythonforandroid/recipes/typing_extensions/__init__.py
+++ pythonforandroid/recipes/typing_extensions/__init__.py
@@ -2,7 +2,7 @@ from pythonforandroid.recipe import PythonRecipe
 
 
 class TypingExtensionsRecipe(PythonRecipe):
-    version = '4.4.0'
+    version = '3.10.0.2'
     url = 'https://pypi.python.org/packages/source/t/typing-extensions/typing_extensions-{version}.tar.gz'
     call_hostpython_via_targetpython = False
     install_in_hostpython = True
