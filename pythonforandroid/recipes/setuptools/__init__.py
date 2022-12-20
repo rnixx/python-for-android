081afb681bfa1b642b4fec4b6cb4cfb894dcbb7d
Robert Niederreiter <rnix@squarewave.at>
2022-12-20 14:26:51 +0100

    setuptools 49.6.0 is a zip file

--- pythonforandroid/recipes/setuptools/__init__.py
+++ pythonforandroid/recipes/setuptools/__init__.py
@@ -3,7 +3,7 @@ from pythonforandroid.recipe import PythonRecipe
 
 class SetuptoolsRecipe(PythonRecipe):
     version = '49.6.0'
-    url = 'https://pypi.python.org/packages/source/s/setuptools/setuptools-{version}.tar.gz'
+    url = 'https://pypi.python.org/packages/source/s/setuptools/setuptools-{version}.zip'
     call_hostpython_via_targetpython = False
     install_in_hostpython = True
 
