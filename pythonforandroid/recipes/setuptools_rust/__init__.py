d0a3370f2fa2e0446396958fd1d91e3118ebc05c
Robert Niederreiter <rnix@squarewave.at>
2022-12-20 12:23:50 +0100

    Fix dependencies

--- pythonforandroid/recipes/setuptools_rust/__init__.py
+++ pythonforandroid/recipes/setuptools_rust/__init__.py
@@ -4,6 +4,7 @@ from pythonforandroid.recipe import PythonRecipe
 class SetuptoolsRustRecipe(PythonRecipe):
     version = '1.5.2'
     url = 'https://pypi.python.org/packages/source/s/setuptools-rust/setuptools-rust-{version}.tar.gz'
+    depends = ['typing_extensions']
     call_hostpython_via_targetpython = False
     install_in_hostpython = True
 
--- pythonforandroid/recipes/typing_extensions/__init__.py
+++ pythonforandroid/recipes/typing_extensions/__init__.py
@@ -4,7 +4,6 @@ from pythonforandroid.recipe import PythonRecipe
 class TypingExtensionsRecipe(PythonRecipe):
     version = '4.4.0'
     url = 'https://pypi.python.org/packages/source/t/typing-extensions/typing_extensions-{version}.tar.gz'
-    depends = ['typing_extensions']
     call_hostpython_via_targetpython = False
     install_in_hostpython = True
 
